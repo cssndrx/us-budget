2c2f1186e09279a24694b2a727993b1bef1dcb43
Cassandra Xia <cssndrx@gmail.com>
2020-05-25 14:53:28 -0400

    Initial commit

---
+++ obama interactive budget/2016 Budget Interactive _ The White House_files/stats.js
@@ -0,0 +1,22 @@
+if(document.images){var img=new Image;img.src=[document.location.protocol,"//web.archive.org/web/20170717231641/https://stats.search.usa.gov/stats.gif?","a=",aid,"&u=",encodeURIComponent(document.URL)].join("")}
+/*
+     FILE ARCHIVED ON 23:16:41 Jul 17, 2017 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 16:37:40 Oct 05, 2019.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  exclusion.robots.policy: 0.217
+  LoadShardBlock: 458.781 (3)
+  load_resource: 1201.244
+  exclusion.robots: 0.233
+  RedisCDXSource: 383.467
+  PetaboxLoader3.datanode: 200.963 (5)
+  PetaboxLoader3.resolve: 1395.832 (4)
+  CDXLines.iter: 46.156 (3)
+  esindex: 0.012
+  captures_list: 1254.529
+*/
